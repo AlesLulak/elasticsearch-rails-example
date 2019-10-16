69d60eb0d592418d521dff4744fe7d5b83bfe46d
Ales Lulak <lulak@insiaoffice.cz>
2019-10-16 16:12:18 +0200

    migrate persons

--- example/config/routes.rb
+++ example/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root "person#index"
+  root "persons#index"
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
