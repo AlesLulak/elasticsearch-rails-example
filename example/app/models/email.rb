06bd52287f3ea6f5eff8e4bf91818dafd1373217
Ales Lulak <lulak@insiaoffice.cz>
2019-10-23 14:10:21 +0200

    add dependent destroy for emails comments

--- example/app/models/email.rb
+++ example/app/models/email.rb
@@ -1,6 +1,6 @@
 class Email < ActiveRecord::Base
   belongs_to :person, touch: true
-  has_many :comments
+  has_many :comments, dependent: :destroy
 
   validates :email, presence: true, uniqueness: true, format: { with: URI::MailTo::EMAIL_REGEXP }
 end
