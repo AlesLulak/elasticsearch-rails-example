93de3455fb723c7b9996e9243ae4512943fe1708
Ales Lulak <lulak@insiaoffice.cz>
2019-11-04 14:17:42 +0100

    add redis for development

--- example/config/environments/development.rb
+++ example/config/environments/development.rb
@@ -1,5 +1,8 @@
 Rails.application.configure do
+  config.action_controller.perform_caching = true
+  config.cache_store = :redis_store, "redis://localhost:6379/0/cache", { expires_in: 90.minutes }
   config.web_console.whitelisted_ips = "172.18.0.1"
+
 
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -13,7 +16,7 @@ Rails.application.configure do
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local = true
-  config.action_controller.perform_caching = false
+  # config.action_controller.perform_caching = false
 
   # Don't care if the mailer can't send.
   config.action_mailer.raise_delivery_errors = false
